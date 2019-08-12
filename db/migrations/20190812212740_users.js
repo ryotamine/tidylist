eee11b7ffabc02c74b6a0bb9998975825dd0f485
Ryota Mine <ryotamine@gmail.com>
2019-08-12 21:41:55 +0000

    Setup initial migrations file

---
+++ db/migrations/20190812212740_users.js
@@ -0,0 +1,13 @@
+"use strict"
+
+exports.up = function(knex) {
+  return knex.schema.createTable("users", function(table) {
+    table.increments("id").primary();
+    table.string("email");
+    table.varchar("password");
+  });
+};
+
+exports.down = function(knex) {
+  knex.schema.dropTable("users");
+};
