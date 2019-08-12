81f39232bfdeff7149ad77c4e976738e226e8b9c
Ryota Mine <ryotamine@gmail.com>
2019-08-12 21:24:48 +0000

    Connect database to express server

--- express_server.js
+++ express_server.js
@@ -7,6 +7,11 @@ const express = require("express");
 const app  = express();
 const PORT = 8080;
 
+// Create AJAX database environment
+const environment   = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database      = require("knex")(configuration);
+
 // Boot server
 app.listen(PORT, () => {
   console.log(`Tidylist app listening on port ${PORT}!`);
