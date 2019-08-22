21084ca9fe0cadf555b5ec909986f65c2e21955c
Ryota Mine <ryotamine@gmail.com>
2019-08-22 19:18:41 +0000

    Modularize code

--- scripts/script.js
+++ scripts/script.js
@@ -6,80 +6,16 @@ const modals = document.querySelectorAll(".modal");
 
 // When the user clicks the button, open the modal
 for (let i = 0; i < btn.length; i++) {
-  btn[i].onclick = function() {
+  btn[i].onclick = () => {
     modals[i].style.display = "block";
   }
 }
 
 // When the users clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target.classList.contains("modal")) {
-    for (let index in modals) {
-      if (typeof modals[index].style !== "undefined") {
-        modals[index].style.display = "none";
-      }
+window.onclick = (e) => {
+  if (e.target.classList.contains("modal")) {
+    for (let i in modals) {
+      modals[i].style.display = "none";
     }
   }
 }
-
-// // Get the register modal
-// const registerModal = document.getElementById("registerModal");
-
-// // Get the register button that opens the modal
-// const registerBtn = document.getElementById("registerBtn");
-
-// // When the user clicks on the register button, open the modal
-// registerBtn.onclick = function() {
-//   registerModal.style.display = "block";
-// }
-
-// // When the user clicks anywhere outside of the register modal, close it
-// window.onclick = function(e) {
-//   if (e.target == registerModal) {
-//     registerModal.style.display = "none";
-//   }
-// }
-
-// // Get the login modal
-// const loginModal = document.getElementById("loginModal");
-
-// // Get the login button that opens the modal
-// const loginBtn = document.getElementById("loginBtn");
-
-// // When the user clicks on the login button, open the modal
-// loginBtn.onclick = function() {
-//   loginModal.style.display = "block";
-// }
-
-// // When the user clicks anywhere outside of the login modal, close it
-// window.onclick = function(e) {
-//   if (e.target == loginModal) {
-//     loginModal.style.display = "none";
-//   }
-// }
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
