81ab37c54af303a0583ef01e142581c49f3b6caa
Ryota Mine <ryotamine@gmail.com>
2019-08-23 20:21:21 +0000

    Allow user to add and remove list items

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,47 @@ window.onclick = (e) => {
     }
   }
 }
+
+// Create a remove button and append it to each list item
+const listItem = document.getElementsByTagName("li");
+for (let i = 0; i < listItem.length; i++) {
+  const span = document.createElement("span");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "remove";
+  span.appendChild(txt);
+  listItem[i].appendChild(span);
+}
+
+// Click on a remove button to delete the current list item
+const remove = document.getElementsByClassName("remove");
+for (let i = 0; i < remove.length; i++) {
+  remove[i].onclick = () => {
+    listItem[i].style.display = "none";
+  }
+}
+
+// When the clicks on the add button, create a new list item
+function newItem() {
+  const li = document.createElement("li");
+  const title = document.getElementById("title").value;
+  const t = document.createTextNode(title);
+  li.appendChild(t);
+  if (title === "") {
+    alert("Please write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("title").value = "";
+
+  const span = document.createElement("span");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "remove";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (let i = 0; i < remove.length; i++) {
+    remove[i].onclick = () => {
+      listItem[i].style.display = "none";
+    }
+  }
+}
